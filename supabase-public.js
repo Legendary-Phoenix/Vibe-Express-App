7a1087bd6225c4177b5e981301332a799ae7a401
Legendary-Phoenix <shobbhan9124@gmail.com>
2025-05-28 13:18:23 +0800

    Removed logs

--- supabase-public.js
+++ supabase-public.js
@@ -4,7 +4,6 @@ dotenv.config();
 
 const supabaseUrl=process.env.SUPABASE_URL;
 const supabaseKey=process.env.SUPABASE_KEY;
-console.log(`Supabase URL: ${supabaseUrl}`);
-console.log(`Supabase Key: ${supabaseKey}`);
+
 export const supabase=createClient(supabaseUrl,supabaseKey);
 
