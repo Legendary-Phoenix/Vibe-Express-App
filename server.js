a694d0e02a16a766110404120d31959c2d4706a1
Legendary-Phoenix <shobbhan9124@gmail.com>
2025-05-28 12:18:56 +0800

    Minor configuration changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { attachSupabase } from "./middleware/auth.js";
 
 dotenv.config();
 const app=express();
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
 
 //middleware
 app.use(cors());
